21fe3ce7a8be0c2ebe6f1bea8927590abadda55d
Ikimoo <peugeot.m.1904@gmail.com>
2024-05-30 18:03:51 +0200

    add asset mapper + bootstrap + css working

--- importmap.php
+++ importmap.php
@@ -16,4 +16,8 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
 ];
